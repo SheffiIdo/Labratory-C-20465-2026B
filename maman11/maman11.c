4798058bbb6accc79f22fb1b01c9077ef0c13c49
Sheffildo <sheffi60@gmail.com>
2026-04-07 12:59:56 +0000

    Add fail details to maman11

--- maman11/maman11.c
+++ maman11/maman11.c
@@ -3,7 +3,8 @@
 int main() {
     /* In ANSI C, declarations MUST come before any executable code */
     int assignment_number = 11;
-
+    // This comment is illegal in ANSI C!
+    printf("This push should fail the CI check.\n");
     printf("Hello, Open University!\n");
     printf("Currently working on Maman %d.\n", assignment_number);
     printf("The Ubuntu 24.04 environment is ready.\n");
