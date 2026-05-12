e98a66d68874d2daa6899fa22224733b893f7bfb
Sheffildo <sheffi60@gmail.com>
2026-05-12 14:27:34 +0000

    Implement full test suite and enforce Valgrind checks on all inputs

--- maman12/difference_and_sum/difference_and_sum.h
+++ maman12/difference_and_sum/difference_and_sum.h
@@ -1,14 +1,11 @@
 #ifndef DIFFERENCE_AND_SUM_H
 #define DIFFERENCE_AND_SUM_H
 
-/* * CONSTANTS
- * Avoiding 'magic numbers' in the source code
- */
 #define PAIR_SIZE 2
 
-/* * MACROS
- * Part B: Macro DIFF calculates the absolute difference.
- * Part C: Macro SUM calculates the sum.
+/* MACROS
+ * Macro DIFF calculates the absolute difference.
+ * Macro SUM calculates the sum.
  */
 #define SUM(a, b) ((a) + (b))
 #define DIFF(a, b) (((a) > (b)) ? ((a) - (b)) : ((b) - (a)))
@@ -19,4 +16,4 @@
 void findDifferenceAndSum(int num1, int num2, int *diff_ptr, int *sum_ptr);
 int* saveArray(int *arr, int *size, int num1, int num2);
 
-#endif /* DIFFERENCE_AND_SUM_H */
+#endif
